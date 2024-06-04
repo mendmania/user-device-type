ee82aafe42188f3ce34c5b4ff79620d6f6ec7738
mendmania <mendmania@gmail.com>
2024-06-04 11:03:10 +0200

    [update] defined client vite config

---
+++ vite.config.client.js
@@ -0,0 +1,13 @@
+import { URL, fileURLToPath } from 'node:url'
+
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+
+})
